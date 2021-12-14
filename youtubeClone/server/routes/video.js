145594a73783910525a5137c4731b400426f70cf
pier101 <85658044+pier101@users.noreply.github.com>
2021-12-15 00:41:03 +0900

    mongoDBconnect, videoUpload & multer

---
+++ youtubeClone/server/routes/video.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+// const { Video } = require("../models/Video");
+
+const { auth } = require("../middleware/auth");
+const multer = require("multer")
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`)
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.mp4' || ext !== '.jpg'|| ext !== '.png') {
+            return cb(res.status(400).end('only jpg, png, mp4 is allowed'), false);
+        }
+        cb(null, true)
+    }
+})
+var upload = multer({ storage: storage }).single("file")
+//=================================
+//             Video
+//=================================
+
+//비디오를 서버에 저장한다.
+router.post("/uploadfiles",(req,res)=>{
+    upload(req, res, err => {
+        if (err) {
+            return res.json({ success: false, err })
+        }
+        return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
+    })
+})
+// router.post("/uploadfiles", (req, res) => {
+
+//     upload(req, res, err => {
+//         if (err) {
+//             return res.json({ success: false, err })
+//         }
+//         return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
+//     })
+
+// });
+module.exports = router;
