251565d6e791c8e6e9b9f25973d00e72d7f3c486
pier101 <85658044+pier101@users.noreply.github.com>
2021-12-17 21:39:38 +0900

    sideVideo, subscription 기능

--- youtubeClone/server/routes/video.js
+++ youtubeClone/server/routes/video.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Video } = require("../models/Video");
-
+const { Subscriber} = require('../models/Subscriber')
 const { auth } = require("../middleware/auth");
 const multer = require("multer")
 var ffmpeg = require('fluent-ffmpeg');
@@ -26,6 +26,8 @@ var upload = multer({ storage: storage }).single("file")
 //             Video
 //=================================
 
+
+
 //비디오를 서버에 저장한다.
 router.post("/uploadfiles",(req,res)=>{
     upload(req, res, err => {
@@ -38,18 +40,15 @@ router.post("/uploadfiles",(req,res)=>{
 
 router.post('/uploadVideo',(req,res)=>{
     //비디오 정보들을 저장한다.
-
-   const video =  new Video(req.body) //받아온 모든 정보 변수에 담기
+    
+    const video =  new Video(req.body) //받아온 모든 정보 변수에 담기
     video.save((err,doc)=>{ //모든 정보 담은 변수 db에 저장
         if(err) return res.json({success:false,err})
         res.status(200).json({success:true})
     }) 
-
-
+    
+    
 })
-
-
-
 
 router.post('/thumbnail', (req,res)=>{
     //썸네일 생성 하고 비디오 러닝타임도 가져오기 >위에 ffmpeg import한다
@@ -86,4 +85,54 @@ router.post('/thumbnail', (req,res)=>{
             filename:'thumbnail-%b.png'
         });
 })
+
+
+router.get("/getVideos", (req, res) => {
+    
+    Video.find()
+    .populate('writer')
+    .exec((err, videos) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).json({ success: true, videos })
+    })
+    
+});
+
+
+router.post("/getVideoDetail", (req, res) => {
+    console.log(req.body.videoId)
+    Video.findOne({ "_id" : req.body.videoId })
+    .populate('writer')
+    .exec((err, video) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).json({ success: true, video })
+    })
+});
+
+
+router.post("/getSubscriptionVideos", (req, res) => {
+    //자신의 아이디를 가지고 구독하는 사람들을 찾는다.
+    Subscriber.find({ 'userFrom': req.body.userFrom })
+    .exec((err, subscribers)=> {
+        if(err) return res.status(400).send(err);
+
+        let subscribedUser = [];
+
+        subscribers.map((subscriber, i)=> {
+            subscribedUser.push(subscriber.userTo)
+        })
+
+
+        // 찾은 사람들의 비디오를 가지고 온다
+        Video.find({ writer: { $in: subscribedUser }})
+            .populate('writer')
+            .exec((err, videos) => {
+                if(err) return res.status(400).send(err);
+                res.status(200).json({ success: true, videos })
+            })
+    })
+});
+
+
+
 module.exports = router;
